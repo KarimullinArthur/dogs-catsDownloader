97ed81f989e7f5a2048d0d4df953a0a9914cbf81
Arthur <KarimullinArthur@engineer.com>
2022-03-26 18:30:05 +0500

    ой в ридере забыли

--- read.py
+++ read.py
@@ -9,14 +9,17 @@ if what == '1':
 
 elif what == '2':
     what = 'cat'
+try:
+    file =  open(f'{what}.json')
+    
+    py = json.load(file)
+    
+    items = len(py)
+    
+    max_indent = len(max(py, key=len)) + 1
+    
+    for i in range(0,items):
+        print(py[i]['life_span'],'\t',py[i]['name'])
 
-file =  open(f'{what}.json')
-
-py = json.load(file)
-
-items = len(py)
-
-max_indent = len(max(py, key=len)) + 1
-
-for i in range(0,items):
-    print(py[i]['life_span'],'\t',py[i]['name'])
+except FileNotFoundError:
+    print('First run load.py')
